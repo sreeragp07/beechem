6978bde6b570fdeb0c1cba2d80e66b48df415fd7
sreeragp07 <sreerag.p772000@lbscek.ac.in>
2025-11-30 12:03:19 +0530

    ui change in personnel list screen

--- lib/screens/personal_list_screen.dart
+++ lib/screens/personal_list_screen.dart
@@ -8,10 +8,12 @@ class PerssonalListScreen extends StatefulWidget {
 }
 
 class _PerssonalListScreenState extends State<PerssonalListScreen> {
+  String status = 'inactive';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           Stack(
@@ -82,181 +84,217 @@ class _PerssonalListScreenState extends State<PerssonalListScreen> {
             ),
           ),
           SizedBox(height: 15),
-          Padding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
-            child: Card(
-              margin: EdgeInsets.zero,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25),
-                side: BorderSide.none,
-              ),
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.amber,
-                          radius: 24,
-                          child: Icon(Icons.group, color: Colors.black87),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    "John Doe",
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Spacer(),
-                                  Container(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 8,
-                                      vertical: 0,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      color: Colors.green.shade100,
-                                      borderRadius: BorderRadius.circular(12),
-                                      border: Border.all(
-                                        width: 1,
-                                        color: Colors.green,
-                                      ),
-                                    ),
-                                    child: SizedBox(
-                                      height: 20,
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            "•", // bold dot
-                                            style: TextStyle(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.w800,
-                                              color: Colors.grey[700],
-                                            ),
-                                          ),
-                    
-                                          SizedBox(width: 5),
-                                          Text(
-                                            "Active",
-                                            style: TextStyle(
-                                              color: Colors.green.shade700,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 10,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.phone,
-                                    size: 16,
-                                    color: Colors.grey[700],
-                                  ),
-                    
-                                  SizedBox(width: 6),
-                    
-                                  Text(
-                                    "9876543210",
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey[700],
-                                    ),
-                                  ),
-                    
-                                  SizedBox(width: 10),
-                    
-                                  Text(
-                                    "•", // bold dot
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey[700],
-                                    ),
-                                  ),
-                    
-                                  SizedBox(width: 10),
-                    
-                                  Icon(
-                                    Icons.person_3_rounded,
-                                    size: 18,
-                                    color: Colors.grey[700],
-                                  ),
-                    
-                                  SizedBox(width: 6),
-                    
-                                  Expanded(
-                                    child: Text(
-                                      "Colony owner",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey[700],
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-
-                    Divider(
-                      thickness: 1,
-                      color: Colors.grey.shade300,
-                      height: 0,
-                    ),
-
-                    SizedBox(height: 8),
-
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.location_on_sharp,
-                          size: 16,
-                          color: Colors.grey,
-                        ),
-
-                        SizedBox(width: 6),
-
-                        Expanded(
-                          child: Text(
-                            "123, MG Road, Bengaluru, Karnataka, 560001, Near Metro Station, Opposite XYZ Mall",
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Colors.grey[700],
-                              height: 1.25
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
+          detailsCard(
+            status: "active",
+            name: "Arun",
+            phone: "956321477410",
+            role: "Land owner",
+            address: "Villa no.13, banglore, karnataka, india, 675643",
+          ),
+          SizedBox(height: 15),
+          detailsCard(
+            status: "inactive",
+            name: "Arun",
+            phone: "956321477410",
+            role: "Land owner",
+            address: "Villa no.13, banglore, karnataka, india, 675643",
+          ),
+          SizedBox(height: 15),
+          detailsCard(
+            status: "inactive",
+            name: "Arun",
+            phone: "956321477410",
+            role: "Land owner",
+            address: "Villa no.13, banglore, karnataka, india, 675643",
+          ),
+          SizedBox(height: 15),
+          detailsCard(
+            status: "active",
+            name: "Arun",
+            phone: "956321477410",
+            role: "Land owner",
+            address: "Villa no.13, banglore, karnataka, india, 675643",
           ),
         ],
       ),
     );
   }
+}
+
+// card widget
+Widget detailsCard({
+  required String status,
+  required String name,
+  required String phone,
+  required String role,
+  required String address,
+}) {
+  return Padding(
+    padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+    child: Card(
+      margin: EdgeInsets.zero,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
+        side: BorderSide.none,
+      ),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  radius: 24,
+                  child: Icon(Icons.group, color: Colors.black87),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 0,
+                            ),
+                            decoration: BoxDecoration(
+                              color: status == 'active'
+                                  ? Colors.green.shade100
+                                  : Colors.red.shade100,
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
+                                width: 1,
+                                color: status == 'active'
+                                    ? Colors.green
+                                    : Colors.red,
+                              ),
+                            ),
+                            child: SizedBox(
+                              height: 20,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "•", // bold dot
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w800,
+                                      color: status == 'active'
+                                          ? Colors.green[700]
+                                          : Colors.red[700],
+                                    ),
+                                  ),
+
+                                  SizedBox(width: 5),
+                                  Text(
+                                    status == 'active' ? "Active" : "Inactive",
+                                    style: TextStyle(
+                                      color: status == 'active'
+                                          ? Colors.green.shade700
+                                          : Colors.red.shade700,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.phone, size: 16, color: Colors.grey[700]),
+
+                          SizedBox(width: 6),
+
+                          Text(
+                            phone,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+
+                          SizedBox(width: 10),
+
+                          Text(
+                            "•", // bold dot
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+
+                          SizedBox(width: 10),
+
+                          Icon(
+                            Icons.person_3_rounded,
+                            size: 18,
+                            color: Colors.grey[700],
+                          ),
+
+                          SizedBox(width: 6),
+
+                          Expanded(
+                            child: Text(
+                              role,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[700],
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+
+            Divider(thickness: 1, color: Colors.grey.shade300, height: 0),
+
+            SizedBox(height: 8),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.location_on_sharp, size: 16, color: Colors.grey),
+
+                SizedBox(width: 6),
+
+                Expanded(
+                  child: Text(
+                    address,
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey[700],
+                      height: 1.25,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
